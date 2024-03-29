d9eff5897d345a5fe83a2e89cb5f4778e13aca63
ElifnurCiftci <elifnur.ciftci20@gmail.com>
2024-03-29 07:50:07 +0300

    recipe front backle bağlandı, submit çalışıyor. dietlerde diet silme yapıldı

--- frontend/src/Pages/recipe.js
+++ frontend/src/Pages/recipe.js
@@ -4,6 +4,8 @@ import { useSpring, animated } from 'react-spring';
 import './Recipe.css';
 import backgroundImage from './background.jpg'
 import Modal from 'react-modal';
+import axios from 'axios';
+import {Rings} from 'react-loader-spinner';
 
 Modal.setAppElement('#root');
 // Dummy data for the recipe suggestion
@@ -26,11 +28,13 @@ Recipe.defaultProps = {
   }
 };
 
-function Recipe({ recipe }) {
+function Recipe() {
   const [liked, setLiked] = useState(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [dislikeReason, setDislikeReason] = useState('');
   const [showRecipe, setShowRecipe] = useState(false);
+  const [recipeData, setRecipeData] = useState({ title: '', instructions:[], ingredients:[] }); // State to store recipe data
+  const [isLoading, setIsLoading] = useState(false); 
 
   // Define the animation for the button
   const [buttonStyle, setButtonStyle] = useSpring(() => ({
@@ -38,9 +42,29 @@ function Recipe({ recipe }) {
     config: { mass: 1, tension: 350, friction: 5 }
   }));
 
+  // animation for button appearance
+  const [buttonAppearStyle, setButtonAppearStyle] = useSpring(() => ({
+    from: { transform: 'scale(0)' },
+    to: { transform: 'scale(1)' },
+    reset: !showRecipe,
+    reverse: showRecipe,
+  }));
+
   // Function to handle button click and show recipe
   const handleButtonClick = () => {
-    setShowRecipe(true);
+    setIsLoading(true);
+    axios.get('http://127.0.0.1:5000/openAiReq').then(response => {
+      setRecipeData({
+        title: response.data.Meal,
+        instructions: response.data.Recipe.Instructions,
+        ingredients: response.data.Recipe.Ingredients
+      });
+      setIsLoading(false);
+      setShowRecipe(true);
+    }).catch(error => {
+      console.error('Error fetching recipe:', error);
+      setIsLoading(false);
+    });
   };
 
   const swipeHandlers = useSwipeable({
@@ -55,6 +79,19 @@ function Recipe({ recipe }) {
     transform: `translateX(${liked === true ? 100 : liked === false ? -100 : 0}px)`
   });
 
+  const handleLike = () => {
+    setLiked(true); // Start the "liked" animation
+  
+    // Wait for the animation to complete before resetting states
+    setTimeout(() => {
+      setLiked(null); // Reset the liked state
+      setShowRecipe(false); // Hide the recipe and show the suggestion button again
+      setTimeout(() => {
+        setButtonAppearStyle({ reset: true });
+      }, 10);
+    }, 200); 
+  };
+
   const handleDislike = () => {
     setLiked(false);
     setModalIsOpen(true);
@@ -62,6 +99,20 @@ function Recipe({ recipe }) {
 
   const closeModal = () => {
     setModalIsOpen(false);
+    setShowRecipe(false); // Hide the recipe
+    setLiked(null); // Reset liked state
+    setDislikeReason('');
+  };
+
+  const handleSubmit = () => {
+    axios.get(`http://127.0.0.1:5000/qdrantSearch?dislikeReason=${encodeURIComponent(dislikeReason)}`)
+      .then(response => {
+        console.log(response.data); // Handle the response data
+        closeModal();
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   };
 
   return (
@@ -73,8 +124,25 @@ function Recipe({ recipe }) {
       width: '100vw',
     height: '100vh'
     }}>
-      {!showRecipe && (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      {isLoading && 
+        <Rings
+        visible={true}
+        height="140"
+        width="140"
+        color="#1f2937"
+        ariaLabel="rings-loading"
+        wrapperStyle={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          backgroundColor: "#f7f7f700",
+        }}
+        wrapperClass=""
+        />
+      }
+      {!showRecipe && !isLoading && (
+        <animated.div style={{ ...buttonAppearStyle, display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
           <animated.button
             style={buttonStyle}
             onMouseEnter={() => setButtonStyle({ transform: 'scale(1.1)' })}
@@ -84,7 +152,7 @@ function Recipe({ recipe }) {
           >
             Tarif öner!
           </animated.button>
-        </div>
+        </animated.div>
       )}
       {showRecipe && <div style={{
           backgroundImage:`url(${backgroundImage})`,
@@ -95,11 +163,25 @@ function Recipe({ recipe }) {
           height: '100vh'}}>
         <animated.div className="Recipe" {...swipeHandlers} style={animProps}
         >
-          <div className="recipe-card">
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
+          <div className="recipe-card" style={{ textAlign: 'left', maxWidth: '600px', margin: 'auto' }}>
+            <h3>{recipeData.title}</h3>
+            <div style={{backgroundColor:"rgb(31, 41, 55)"}}>
+            <h4 style={{paddingLeft:"20px", color:"#fff"}}>Ingredients:</h4>
+            <ul style={{paddingLeft:"30px", paddingRight:"10px", color:"#fff"}}>
+              {recipeData.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+
+            <h4 style={{paddingLeft:"20px", color:"#fff"}}>Instructions:</h4>
+            <ol style={{paddingLeft:"30px" , paddingRight:"10px", color:"#fff"}}>
+              {recipeData.instructions.map((step, index) => (
+                <li key={index}>{step}</li>
+              ))}
+            </ol>
+            </div>
             <div className="buttons">
-              <button onClick={() => setLiked(true)} className="yes-button">Yes</button>
+              <button onClick={handleLike} className="yes-button">Yes</button>
               <button onClick={handleDislike} className="no-button">No</button>
             </div>
           </div>
@@ -120,7 +202,7 @@ function Recipe({ recipe }) {
             value={dislikeReason}
             onChange={(e) => setDislikeReason(e.target.value)}
           />
-          <button className='submit-button' onClick={closeModal}>Submit</button>
+          <button className='submit-button' onClick={handleSubmit}>Submit</button>
         </Modal>
         </div>
       }
